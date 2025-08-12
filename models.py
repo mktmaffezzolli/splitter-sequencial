aa2e5adac585a6bc0688ae62f0f075898cfc2fbe
mktmaffezzolli <mktmaffezzolli@gmail.com>
2025-08-12 12:12:55 -0300

    Create models.py

---
+++ models.py
@@ -0,0 +1,15 @@
+from app import db
+from datetime import datetime
+
+class URLSplit(db.Model):
+    __tablename__ = 'url_splits'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    slug = db.Column(db.String(100), unique=True, nullable=False)
+    name = db.Column(db.String(200), nullable=False)
+    destinations = db.Column(db.Text, nullable=False)  # JSON array
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    
+    def __repr__(self):
+        return f'<URLSplit {self.slug}>'
